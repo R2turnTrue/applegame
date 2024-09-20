95293acd60c1407c673f02f43727cce8d9d1b6f4
R2turnTrue <46389044+R2turnTrue@users.noreply.github.com>
2024-09-21 03:16:46 +0900

    first commit

---
+++ solve.py
@@ -0,0 +1,100 @@
+import pyautogui
+import time
+
+#pyautogui.PAUSE = 0
+
+# w=17, h=10 board
+
+BOARD_OFFSET_START = (2563, 167) # hardcoded value
+
+# board[y][x]
+board = []
+
+for y in range(10):
+    board.append(list(map(int, input().split())))
+
+def solve(x_start, y_start, width, height):
+    x_off = BOARD_OFFSET_START[0] + 32.7 * x_start
+    y_off = BOARD_OFFSET_START[1] + 32.3 * y_start
+    pyautogui.moveTo(x_off, y_off)
+    pyautogui.mouseDown(button='left')
+    pyautogui.moveTo(x_off + 32.7 * width, y_off + 32.3 * height, duration=0.25)
+    time.sleep(0.05)
+    pyautogui.moveTo(x_off + 32.7 * width + 5, y_off + 32.3 * height + 5, duration=0.1)
+    time.sleep(0.1)
+    pyautogui.mouseUp(button='left')
+
+print('solving')
+
+total_solutions = 0
+
+while True:
+    solve_count = 0
+
+    for y in range(10):
+        for x in range(17):
+            #print('===newcell')
+            #print('{}/{}'.format(x, y))
+            add = 0
+            height = 1
+            width = 1
+            while True:
+                if x + width >= 17 + 1:
+                    break
+                for xx in range(x, x + width):
+                    num = board[y][xx]
+                    add += num
+                
+                if add > 10:
+                    break
+
+                add = 0
+
+                while True:
+                    #print('///it')
+                    if x + width >= 17 + 1:
+                        #print('BREAK! (w)')
+                        break
+                    if y + height >= 10 + 1:
+                        #print('BREAK! (h)')
+                        break
+                    
+                    for xx in range(x, x + width):
+                        for yy in range(y, y + height):
+                            num = board[yy][xx]
+
+                            add += num
+
+                            #print('{}/{}/{}'.format(x, yy, add))
+                    
+                    if add >= 10:
+                        break
+
+                    height += 1
+                    add = 0
+                
+                if add > 10:
+                    pass
+                elif add == 10: # add == 10
+
+                    for xx in range(x, x + width):
+                        for yy in range(y, y + height):
+                            board[yy][xx] = 0
+
+                    print('min: {} {} / max: {} {}'.format(x + 1, y + 1, x + 1 + width - 1, y + 1 + height - 1))
+
+                    solve(x, y, width, height)
+
+                    solve_count += 1
+                    total_solutions += 1
+                    break
+                
+                add = 0
+                width += 1
+                height = 1
+    
+    if solve_count == 0:
+        for y in range(10):
+            print(' '.join(map(str, board[y])))
+        print('Solve end with total solutions: {}'.format(total_solutions))
+        break
